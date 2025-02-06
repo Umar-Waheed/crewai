8f88cdf2ca0c4766c53da005f6f99300879a955f
Umar-Waheed <umarwaheed86@outlook.com>
2025-02-07 01:50:23 +0500

    crewai project

---
+++ project1/src/project1/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from crewai.flow.flow import Flow, listen, start
+from dotenv import load_dotenv
+from litellm import completion
+
+
+class ExampleFlow(Flow):
+    model = "gemini/gemini-1.5-flash"
+
+    @start()
+    def generate_country(self):
+        print("Starting flow")
+        # Each flow state automatically gets a unique ID
+        print(f"Flow State ID: {self.state['id']}")
+
+        response = completion(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Return the name of a random country in the world.",
+                },
+            ],
+        )
+
+        random_country = response["choices"][0]["message"]["content"]
+        # Store the city in our state
+        self.state["city"] = random_country
+        print(f"Random City: {random_country}")
+
+        return random_country
+
+    @listen(generate_country)
+    def generate_fun_fact(self, random_country):
+        response = completion(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Tell me a fun fact about {random_country}",
+                },
+            ],
+        )
+
+        fun_fact = response["choices"][0]["message"]["content"]
+        # Store the fun fact in our state
+        self.state["fun_fact"] = fun_fact
+        return fun_fact
+
+
+def calling():
+    flow = ExampleFlow()
+    result = flow.kickoff()
+    print(f"Generated fun fact: {result}")
